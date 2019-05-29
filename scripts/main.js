ae657041d5014936bdef7fc23ce07f571e9ac795
Calvin Golas <32349728+CalvinGolas@users.noreply.github.com>
2019-05-29 14:28:49 -0600

    Updated script to cat theme!

--- scripts/main.js
+++ scripts/main.js
@@ -13,13 +13,13 @@ var myHeading = document.querySelector('h1');
 function setUserName() {
     var myName = prompt('Please enter your name.');
     localStorage.setItem('name', myName);
-    myHeading.textContent = 'Welcome to Spooktober, ' + myName;
+    myHeading.textContent = 'Welcome to Catacular, ' + myName;
 }
 if(!localStorage.getItem('name')) {
     setUserName();
 } else {
     var storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Welcome to Spooktober, ' + storedName;
+    myHeading.textContent = 'Welcome to Catacular, ' + storedName;
 }
 myButton.onclick = function() {
     setUserName();
